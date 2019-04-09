d2d88920bdcdb6b6fc66de05bc65ef919231031d
LaupWing <loc_strict@hotmail.com>
2019-04-09 22:06:54 +0200

    goo

--- project/src/index.js
+++ project/src/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+// const http = require('http').Server(app);
 const port = process.env.port || 4000;
-const router = require('./router/routes');
-const io = require('socket.io')(http);
+const server = app.listen(port, ()=>console.log(`Server is running on port ${port}`))
+// In the second parameter is the server you want to work with in this case
+// it is the app server
+const io = require('socket.io')(server);
    
 app
-    .set('views', 'view')
-    .set('view engine', 'ejs')
-    .use(router)
     .use(express.static('static'));
 
+// The variable io is gonna listen to a event called connection
+// Every client has an instance of a socket that has been made
+// so every client has a diffrent socket 
+const array = io.sockets.clients()
 io.on('connection', function(socket){
-    console.log('a user connected');
+    console.log('a user connected', socket.id);
     socket.on('disconnect', function(){
         console.log('user disconnected');
       });
@@ -23,6 +26,10 @@ io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
       });
+    
+    socket.on('set name', function(name){
+        socket.emit('set name', name)
+    })
     });
 
-http.listen(port, ()=>console.log(`Server is running on port ${port}`));
+// http.listen(port, ()=>console.log(`Server is running on port ${port}`));
--- project/static/script/socket.js
+++ project/static/script/socket.js
@@ -1,16 +1,39 @@
+const socket = io();
 (function(){
-    const socket = io();
-    document.querySelector('form')
+    document.querySelector('#intro form')
         .addEventListener('submit', (e)=>{
             e.preventDefault();
-            console.log(document.querySelector('#m').value)
-            socket.emit('chat message', document.querySelector('#m').value);
-            document.querySelector('#m').value = ''
+            socket.emit('set name', document.querySelector('input').value);
+            document.querySelector('input').value = ''
+            removeElements(document.body)
         })
-    socket.on('chat message', function(msg){
-        const node = document.createElement('li')
-        const textNode = document.createTextNode(msg)
-        node.appendChild(textNode)
-        document.querySelector('#messages').appendChild(node)
-    })
 }())
+    
+socket.on('chat message', function(msg){
+    const node = document.createElement('li')
+    const textNode = document.createTextNode(msg)
+    node.appendChild(textNode)
+    document.querySelector('#messages').appendChild(node)
+})
+
+socket.on('set name', function(name){
+const element = `<ul id="messages"></ul><form id="chat" action=""><h2>${name}</h2><input id="m" autocomplete="off" /><button>Send</button></form>`
+    document.body.insertAdjacentHTML('beforeend', element)
+    nameDefined()
+})
+
+
+function removeElements(container){
+    while(container.firstChild){
+        container.removeChild(container.firstChild)
+    }
+}
+
+function nameDefined(){
+    document.querySelector('#chat')
+        .addEventListener('submit', (e)=>{
+            e.preventDefault()
+            socket.emit('chat message', document.querySelector('input').value)
+            document.querySelector('input').value = ''
+        })
+}
